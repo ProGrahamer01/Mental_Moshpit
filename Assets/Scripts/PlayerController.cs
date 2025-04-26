89ab852f70c253ae1acb0a745bfaf07db6ba223d
Justyce Graham <prograhamer01@gmail.com>
2025-04-26 15:05:26 -0400

    UI has been created
    This update is coming from my macbook instead of my desktop. The ui showing the number of cubes collected and a you win text object that appears only when a specific number of cubes has been selected

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
     
     public class PlayerController : MonoBehaviour
 {
 
-    // variable assignments
+    // ** variable assignments **
 
     // rigidbody of the player
     private Rigidbody rb;
     
+    //declaring the count variable for later loop
+    private int count = 0;
     // movement along x and y axes
     private float movementX, movementY;
 
     // Speed that the player moves at (editble from the inspector) 
     public float speed = 0;
 
+    // This holds a refrence to the UItext component
+    public TextMeshProUGUI countText;
+
+    // UI object to display winning text.
+    public GameObject WinTextObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
     // Get and store the rigidbody component attached to the player 
         rb = GetComponent<Rigidbody>();
+
+    // at the start of the game returns value to 0
+        count = 0;
+
+    // updates the text each time the count variable increases 
+        SetCountText();
+
+     // Initially set the win text to be inactive. 
+        WinTextObject.SetActive(false);
+
         
     }
 
@@ -36,6 +55,21 @@ using UnityEngine.InputSystem;
         movementX = movementVector.x;
         movementY = movementVector.y;
     
+    }
+    
+    // Function to update the displayed count of "PickUp" objects collected.
+    void SetCountText()
+    { 
+        // Update the count text with the current count.
+        countText.text = "count: " + count.ToString();
+
+         // Check if the count has reached or exceeded the win condition.
+        if (count >= 16)
+        {
+             // Display the win text.
+
+            WinTextObject.SetActive(true);
+        }
     }
 
     //This function is incharge of udating the game with the above settings
@@ -56,6 +90,11 @@ using UnityEngine.InputSystem;
             // Deactivate the collided object (making it disappear).
             other.gameObject.SetActive(false);
 
+            //incrementing count after collectible picked up
+            count = count + 1;
+
+            // updates the text each time the count variable increases 
+            SetCountText();
         }
     }
 
